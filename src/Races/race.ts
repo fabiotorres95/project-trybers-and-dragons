eb3c27307d68e9362fa8c2150d3447ebdd2f2fce
fabiotorres95 <engfabiotorres@outlook.com>
2023-08-22 14:48:03 -0300

    creatd class Race, getters implemented

--- src/Races/race.ts
+++ src/Races/race.ts
@@ -1,2 +1,11 @@
 export default class Race {
+  constructor(private _name: string, private _dexterity: number) {}
+
+  public get name() {
+    return this._name;
+  }
+
+  public get dexterity() {
+    return this._dexterity;
+  }
 }
